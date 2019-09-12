cd6b5ec5e34005b2084143a8501bf1f7a1e0f820
Zach Stott <spywoawe@gmail.com>
2019-09-13 09:18:09 +1200

    Base Scripts

---
+++ New Unity Project/Assets/_Scripts/CastleHP.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CastleHP : MonoBehaviour
+{
+    public float castleHP = 10;
+    public float castle2HP = 10;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ New Unity Project/Assets/_Scripts/Cooldown.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cooldown : MonoBehaviour
+{
+    public float cooldownTime = 2;
+    float nextFireTime = 0;
+    public float cooldownTime2 = 2;
+    float nextFireTime2 = 0;
+    public GameObject stickUnit;
+    public GameObject stickUnit2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > nextFireTime)
+        {
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                print("Spawned unit. Cooldown started.");
+                nextFireTime = Time.time + cooldownTime;
+                Instantiate(stickUnit);
+            }
+        }
+
+
+        if (Time.time > nextFireTime2)
+        {
+            if (Input.GetKeyDown(KeyCode.RightShift))
+            {
+                print("Spawned unit. Cooldown started.");
+                nextFireTime2 = Time.time + cooldownTime2;
+                Instantiate(stickUnit2);
+            }
+        }
+    }
+}
