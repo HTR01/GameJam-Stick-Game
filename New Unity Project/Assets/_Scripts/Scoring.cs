92397bf3cfb85222c814fb5c558efcae15b0150f
Zach Stott <spywoawe@gmail.com>
2019-09-13 11:39:35 +1200

    Castle HP Working

---
+++ New Unity Project/Assets/_Scripts/Scoring.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scoring : MonoBehaviour
+{
+    public string teamname;
+    public HPManager HPMan;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+
+
+        if (teamname == "Blue")
+        {
+            HPMan.Castle1HP();
+        }
+
+        if (teamname == "Red")
+        {
+            HPMan.Castle2HP();
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
