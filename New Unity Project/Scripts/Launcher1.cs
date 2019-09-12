8198e6a0784613ce6d9f8f7be2061e87ccbd89f2
Donnavan <donnavanw@live.com>
2019-09-13 09:27:13 +1200

    Test

---
+++ New Unity Project/Scripts/Launcher1.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Launcher1 : MonoBehaviour
+{
+
+
+
+    public Transform LaunchPoint;
+    public GameObject UnitPrefab;
+
+
+
+
+
+
+ 
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            Launch();
+        }
+        
+
+      
+    }
+
+
+
+
+    void Launch()
+    {
+        Instantiate(UnitPrefab, LaunchPoint.position, LaunchPoint.rotation);
+    }
+}
---
+++ New Unity Project/Scripts/Launcher2.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Launcher2 : MonoBehaviour
+{
+
+
+
+    public Transform LaunchPoint;
+    public GameObject UnitPrefab;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.RightShift))
+        {
+            Launch();
+        }
+    }
+
+
+
+    void Launch()
+    {
+        Instantiate(UnitPrefab, LaunchPoint.position, LaunchPoint.rotation);
+    }
+}
