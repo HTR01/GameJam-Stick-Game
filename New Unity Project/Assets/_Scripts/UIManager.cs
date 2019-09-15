16f1ef49e371afee797fe2907b675b3e917beaf2
Zach Stott <spywoawe@gmail.com>
2019-09-16 09:13:14 +1200

    Timer

--- New Unity Project/Assets/_Scripts/UIManager.cs
+++ New Unity Project/Assets/_Scripts/UIManager.cs
@@ -7,17 +7,21 @@ public class UIManager : MonoBehaviour
 {
     public Text castle1HP;
     public Text castle2HP;
+    public Text timer;
 
     // Start is called before the first frame update
     void Start()
     {
         castle1HP.text = "5";
         castle2HP.text = "5";
+        timer.text = "0";
     }
 
     // Update is called once per frame
     void Update()
     {
+        timer.text = "" + Mathf.RoundToInt(Time.time);
+
         if (castle1HP.text == "0")
         {
 
