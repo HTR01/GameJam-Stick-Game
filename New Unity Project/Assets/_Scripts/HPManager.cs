68766e8cafbe2dfed5f84147bc312da473b05a8a
Zach Stott <spywoawe@gmail.com>
2019-09-16 08:52:53 +1200

    Win reset

--- New Unity Project/Assets/_Scripts/HPManager.cs
+++ New Unity Project/Assets/_Scripts/HPManager.cs
@@ -2,17 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class HPManager : MonoBehaviour
 {
     public int castle1HP;
     public int castle2HP;
     private UIManager uiMan;
+    public GameObject Win1;
+    public GameObject Win2;
 
     // Start is called before the first frame update
     void Start()
     {
         uiMan = GetComponent<UIManager>();
+        Time.timeScale = 1;
     }
 
     public void Castle1HP()
@@ -30,6 +34,18 @@ public class HPManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (castle1HP == 0)
+        {
+            Win1.SetActive(true);
+            Time.timeScale = 0;
+            SceneManager.LoadScene(1);
+        }
+
+        if (castle2HP == 0)
+        {
+            Win2.SetActive(true);
+            Time.timeScale = 0;
+            SceneManager.LoadScene(1);
+        }
     }
 }
--- New Unity Project/Assets/_Scripts/_StartScreen.cs
+++ New Unity Project/Assets/_Scripts/_StartScreen.cs
@@ -8,7 +8,7 @@ public class _StartScreen : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Time.timeScale = 1;
     }
 
     // Update is called once per frame
